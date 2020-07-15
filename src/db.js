051154ad53c2cb7410ca02ddee46540f43a204f9
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-07-15 18:40:36 +0200

    fixed problems

--- src/db.js
+++ src/db.js
@@ -1,7 +1,8 @@
 //ABYME single module to manage connection to neo4j
-import Neode from 'neode';
+// import Neode from 'neode';
+const Neode = require('neode')
 
-const neode
+let neode = null
 
 if (process.env.NODE_ENV === 'production') {
     const neode = new Neode(process.env.GRAPHENEDB_BOLT_URL, process.env.GRAPHENEDB_BOLT_USER, process.env.GRAPHENEDB_BOLT_PASSWORD, true);
