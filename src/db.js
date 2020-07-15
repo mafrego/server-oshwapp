648b10f6fd0520eba3961362066e9b77e1668529
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-07-15 18:32:40 +0200

    modified db.js for production

--- src/db.js
+++ src/db.js
@@ -1,13 +1,17 @@
 //ABYME single module to manage connection to neo4j
-console.log(process.env.NEO4J_PROTOCOL)
-console.log(process.env.NEO4J_HOST)
-console.log(process.env.NEO4J_USERNAME)
-console.log(process.env.NEO4J_PASSWORD)
-console.log("ciao")
+import Neode from 'neode';
 
-var neode = require('neode')
-            .fromEnv()
-            .withDirectory(__dirname+'/models');
+const neode
+
+if (process.env.NODE_ENV === 'production') {
+    const neode = new Neode(process.env.GRAPHENEDB_BOLT_URL, process.env.GRAPHENEDB_BOLT_USER, process.env.GRAPHENEDB_BOLT_PASSWORD, true);
+    neode.withDirectory(__dirname+'/models');
+} else {
+    neode = Neode
+        // require('neode')
+        .fromEnv()
+        .withDirectory(__dirname + '/models');
+}
 
 console.log('connected to neo4j through neode')
 
