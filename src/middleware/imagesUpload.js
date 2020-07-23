e63aedc417ace7caac01f33f5dbcfb540114c91f
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-07-23 09:18:32 +0200

    figa...

--- src/middleware/imagesUpload.js
+++ src/middleware/imagesUpload.js
@@ -3,14 +3,14 @@ const sharp = require("sharp");
 const aws = require("aws-sdk");
 const db = require('../db.js')
 
-const s3 = new aws.S3({
-  accessKeyId: process.env.AWSAccessKeyId,
-  secretAccessKey: process.env.AWSSecretKey
-});
+// const s3 = new aws.S3({
+//   accessKeyId: process.env.AWSAccessKeyId,
+//   secretAccessKey: process.env.AWSSecretKey
+// });
 
 const BUCKET_NAME = process.env.BUCKET_NAME
-const AWSAccessKeyId = process.env.AWSSecretKey
-const AWSSecretKey = process.env.AWSSecretKey
+// const AWSAccessKeyId = process.env.AWSSecretKey
+// const AWSSecretKey = process.env.AWSSecretKey
 
 
 // if (process.env.NODE_ENV === 'production') {
@@ -19,9 +19,9 @@ const AWSSecretKey = process.env.AWSSecretKey
 //   console.log(process.env.AWSSecretKey)
 //   console.log(process.env.BUCKET_NAME)
 
-//   aws.config = new aws.Config();
-//   aws.config.accessKeyId = process.env.AWSAccessKeyId
-//   aws.config.secretAccessKey = process.env.AWSSecretKey
+  aws.config = new aws.Config();
+  aws.config.accessKeyId = process.env.AWSAccessKeyId
+  aws.config.secretAccessKey = process.env.AWSSecretKey
 //   // aws.config.region = 'eu-central-1'
 // } else {
 //   aws.config.update({
@@ -89,7 +89,7 @@ const resizeAndUploadToS3Images = async (req, res, next) => {
     const folderName = req.params.projectId
     req.body.images = [];
     req.body.s3responses = []
-    // const s3 = new aws.S3()
+    const s3 = new aws.S3()
 
     await Promise.all(
       req.files.map(async file => {
