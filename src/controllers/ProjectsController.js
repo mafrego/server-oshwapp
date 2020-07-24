ea56d27da94b8f7faa9d41a2b826b550877c0521
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-07-24 23:52:00 +0200

    delete nodes and images together with project

--- src/controllers/ProjectsController.js
+++ src/controllers/ProjectsController.js
@@ -1,4 +1,22 @@
 const db = require('../db.js');
+const aws = require("aws-sdk");
+
+const BUCKET_NAME = process.env.BUCKET_NAME
+const AWSAccessKeyId = process.env.AWSAccessKeyId
+const AWSSecretKey = process.env.AWSSecretKey
+
+if (process.env.NODE_ENV === 'production') {
+    aws.config = new aws.Config();
+    aws.config.accessKeyId = AWSAccessKeyId
+    aws.config.secretAccessKey = AWSSecretKey
+    aws.config.region = 'eu-central-1'
+} else {
+    aws.config.update({
+        accessKeyId: AWSAccessKeyId,
+        secretAccessKey: AWSSecretKey,
+        region: 'eu-central-1'
+    })
+}
 
 module.exports = {
 
@@ -28,24 +46,24 @@ module.exports = {
             });
         }
     },
-    
+
     //it works
-    async showBom(req, res){
+    async showBom(req, res) {
         const project = db.model('Project')
         try {
             const bom = await project.find(req.query.projectID)
                 .then(ret => {
-                    console.log('ret: '+ret)
+                    console.log('ret: ' + ret)
                     return ret.toJson()
                 })
-                .then(pro =>{
+                .then(pro => {
                     console.log(pro)
                     // get all user projects and assign them to array pros
                     const arr = pro.consists_of.map(rel => rel.node);
-                    console.log('arr: '+arr)
+                    console.log('arr: ' + arr)
                     return arr
                 })
-            console.log('bom: '+bom)
+            console.log('bom: ' + bom)
             res.send(bom)
         } catch (error) {
             console.log(error);
@@ -57,6 +75,7 @@ module.exports = {
 
     async createProject(req, res) {
         try {
+            // TODO compose imageUrl from env. variable and user image or other
             req.body.imageUrl = "https://oshwapp.s3.eu-central-1.amazonaws.com/service/project.svg"
             const ret = await db.model('Project').create(req.body)
             const project = await ret.toJson()
@@ -86,11 +105,11 @@ module.exports = {
         }
     },
 
-    async updateUser(req, res){
+    async updateUser(req, res) {
         try {
             let user = null
             user = await db.model('User')
-                            .mergeOn(req.userID, { manages: {properties: {status: 'created'}, node: req.projectID}})
+                .mergeOn(req.userID, { manages: { properties: { status: 'created' }, node: req.projectID } })
             console.log(user)
         } catch (error) {
             console.log(error);
@@ -100,11 +119,49 @@ module.exports = {
         }
     },
 
+    // delete all relative products and images in s3
     async delete(req, res) {
         try {
-            const product = await db.model('Project').find(req.params.id)
-            await product.delete()
-            console.log('project deleted');
+            // nodes with relationships consist_of and refers_to to delete 
+            const project = await db.model('Project').find(req.params.id)
+            const projectJson = await project.toJson()
+            const atoms = projectJson.consists_of.map(el => el.node)
+            await Promise.all(
+                atoms.map(async atom => {
+                    try {
+                        const atomToDelete = await db.model('Atom').find(atom.uuid)
+                        await atomToDelete.delete()
+                        // console.log(`deleted atom with uuid:${atom.uuid}`)
+                    } catch (error) {
+                        console.log(error)
+                        res.status(500).send({
+                            error: `An error has occured trying to delete the atom with uuid:${atom.uuid}`
+                        });
+                    }
+                })
+            )
+            // TODO replicate code above for assemblies
+
+            // array of objects(imagename.png) to delete on s3
+            const imageNames = projectJson.consists_of.map(el => el.node.name)
+            const Objects = imageNames.map(name => ({ Key: req.params.id + "/" + name + ".png" }))
+            console.log(Objects)
+            const toDelete = {
+                Bucket: BUCKET_NAME,
+                Delete: {
+                    Objects: Objects,
+                    Quiet: false
+                }
+            }
+
+            const s3 = new aws.S3()
+            s3.deleteObjects(toDelete, function (err, data) {
+                if (err) console.log(err, err.stack)
+                else console.log(data)
+            })
+            // finally delete project node itself
+            await project.delete()
+            // console.log(`deleted project with uuid:${req.params.id}`);
             res.status(200).send({ msg: `project with uuid:${req.params.id} has been deleted` });
         } catch (error) {
             console.log(error);
