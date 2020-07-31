7ac3edfc3c9958216f4a2bdd393c45cbbf87b1bf
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-07-31 17:52:50 +0200

    logic for reducing quantity_to_assemble

--- src/controllers/AssembliesController.js
+++ src/controllers/AssembliesController.js
@@ -13,7 +13,7 @@ module.exports = {
                 assemblies = await ret.toJson()
             } else {
                 const ret = await db.all('Assembly')
-                assemblies = await ret.toJson() 
+                assemblies = await ret.toJson()
             }
             res.status(200).send(assemblies)
         } catch (err) {
@@ -46,7 +46,7 @@ module.exports = {
                 {
                     assembled_from: req.body.parts.map((uuid, index) => ({
                         quantity: req.body.quantities[index],
-                        node: uuid 
+                        node: uuid
                     }))
                 })
             const json = await ret.toJson()
@@ -62,6 +62,7 @@ module.exports = {
     // TODO refactor and find a way to display only the remaining parts to be assembled
     async assemble(req, res) {
         const projectId = req.params.id
+        // console.log(req.body.quantities)
         try {
             const assembly = await db.mergeOn('Assembly',
                 req.body,
@@ -73,16 +74,36 @@ module.exports = {
                 })
                 .then(assembly => { return assembly.toJson() })
 
-                // link project to new assembly
+            // link project to new assembly
             db.mergeOn('Project',
                 { uuid: projectId },
-                { refers_to: [{ 
-                    node: assembly.uuid, 
-                    type: req.body.type, 
-                    version: req.body.version, 
-                    quantity_to_assemble: req.body.quantity_to_assemble
-                }] }
+                {
+                    refers_to: [{
+                        node: assembly.uuid,
+                        type: req.body.type,
+                        version: req.body.version,
+                        // quantity_to_assemble: req.body.quantity_to_assemble
+                    }]
+                }
             )
+
+            // update quantity_to_assemble for each children of newly created assembly
+            await Promise.all(req.body.parts.map(async (uuid, index) => {
+                try {
+                    const product = await db.find('Product', uuid )
+                    const json = await product.toJson()
+                    const quantity_to_assemble = json.quantity_to_assemble
+                    const updated_quantity = quantity_to_assemble - req.body.quantities[index]
+                    await db.mergeOn('Product', { uuid: uuid }, { quantity_to_assemble: updated_quantity })
+                } catch (error) {
+                    console.log(error)
+                    res.status(500).send({
+                        error: `An error has occured trying to update quantity_to assemble of Product with uuid:${uuid}`
+                    });
+                }
+            })
+            )
+
             res.status(201).send(assembly)
             // console.log(assembly)
         } catch (error) {
