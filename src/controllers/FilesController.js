625189ff451c2f91a1c27f18e66c2b0d54c942e8
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-08-02 20:32:23 +0200

    removed csv validation logic

--- src/controllers/FilesController.js
+++ src/controllers/FilesController.js
@@ -1,92 +1,32 @@
 const db = require('../db.js');
-const CSVFileValidator = require('csv-file-validator')
-const fs = require('fs');
-
-// TODO refine config with all proper error functions and options
-const config = {
-    headers: [
-        {
-            name: 'atom name',
-            inputName: 'name',
-            required: true,
-            requiredError: function (headerName, rowNumber, columnNumber) {
-                return `${headerName} is required in the ${rowNumber} row / ${columnNumber} column`
-            },
-            unique: true,
-            uniqueError: function (headerName) {
-                return `${headerName} is not unique`
-            }
-        },
-        {
-            name: 'atom description',
-            inputName: 'description',
-            required: false
-        },
-        {
-            name: 'quantity',
-            inputName: 'quantity',
-            required: false,
-        },
-        {
-            name: 'material',
-            inputName: 'material',
-            required: false
-        },
-        {
-            name: 'weight',
-            inputName: 'weight',
-            required: false
-        },
-        {
-            name: 'weight unit',
-            inputName: 'weightUnit',
-            required: false
-        }
-    ]
-}
 
 module.exports = {
 
-    async manageFile(req, res) {
+    // use as middleware function for storing atoms before calling getBom() in ProjectController
+    async storeBom(req, res) {
         try {
-            let stream = fs.createReadStream(req.file.path);
-            // validate BOM.csv file
-            const result = await CSVFileValidator(stream, config)
-                .then(ret => {
-                    fs.unlinkSync(req.file.path);       //remove file
-                    return ret;
-                })
-            if (result.inValidMessages.length) {
-                console.log(result.inValidMessages)
-                throw result.inValidMessages
-            } else {
-                // eliminate first element of result.data array because is empty object
-                result.data.shift()
-                // array of atom objects
-                const atoms = result.data
-                const projectId = req.params.projectId
-                atoms.forEach(element => {
-                    // add quantity_to_assemble
-                    element.quantity_to_assemble = element.quantity
-                    // it works: given Project create relationship to and node Atom
-                    db.mergeOn('Project',
-                        { uuid: projectId },
-                        {
-                            consists_of: [{ 
-                                // quantity_to_assemble: element.quantity, 
-                                node: element 
-                            }]
-                        }
-                    )
-                });
-                res.status(201).send({
-                    message: 'Atoms of BOM created'
-                })
-            }
+            // eliminate first element of result.data array because is empty object
+            req.result.data.shift()
+            // array of atom objects from middleware
+            const atoms = req.result.data
+            const projectId = req.params.projectId
+            atoms.forEach(async element => {
+                element.quantity_to_assemble = element.quantity
+                // it works: given Project create relationship to and node Atom
+                await db.mergeOn('Project',
+                    { uuid: projectId },
+                    {
+                        consists_of: [{
+                            node: element
+                        }]
+                    }
+                )
+            });
+            res.status(201).send({ msg: 'bom uploaded!' })
         } catch (error) {
-            console.log(error);
+            console.log(error)
             res.status(500).send({
-                error: error
+                error: 'An error has occurred trying to store bom on db'
             })
         }
     },
@@ -94,7 +34,7 @@ module.exports = {
     async uploadImages(req, res) {
         try {
             if (req.results.length <= 0) {
-                return res.status(200).send({message: 'no images uploaded'});
+                return res.status(200).send({ message: 'no images uploaded' });
             }
             res.status(201).send({
                 message: 'Images uploaded!',
@@ -106,13 +46,13 @@ module.exports = {
     },
 
     // just for testing
-    testSingleFile(req, res){
+    testSingleFile(req, res) {
         console.log(req.body)
         res.status(200).send({
             message: 'single file arrived!'
         })
     },
-    testMultipleFiles(req, res){
+    testMultipleFiles(req, res) {
         console.log(req.body)
         res.status(200).send({
             message: 'multiple files arrived!'
