b444e20505aff023213f58bab7fae4c6ca63b85b
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-08-09 15:05:00 +0200

    created

---
+++ src/routes/AdminRoutes.js
@@ -0,0 +1,47 @@
+const AdminController = require('../controllers/AdminController')
+const ProjectsController = require('../controllers/ProjectsController')
+const authJwt  = require("../middleware/AuthJWT");
+
+module.exports = (app) => {
+    
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+    app.get('/allusers',
+    authJwt.verifyToken,
+    authJwt.isAdmin,
+    AdminController.getAllUsers)
+
+    app.get('/allprojects',
+    authJwt.verifyToken,
+    authJwt.isAdmin,
+    AdminController.getAllProjects)
+
+    app.delete('/deleteuser/:id', 
+    authJwt.verifyToken,
+    authJwt.isAdmin,
+    AdminController.deleteUser)
+
+    // app.get('/atoms/:id', 
+    // AtomsController.show)
+
+    // app.post('/atoms', 
+    // authJwt.verifyToken,
+    // authJwt.isAssemblerOrAdmin,
+    // AtomsController.post)
+
+    // app.delete('/deleteproject/:id', 
+    // authJwt.verifyToken,
+    // authJwt.isAdmin,
+    // AdminController.deleteProject)
+
+    app.delete('/deleteproject/:id', 
+    authJwt.verifyToken,
+    authJwt.isAdmin,
+    ProjectsController.delete)
+}
