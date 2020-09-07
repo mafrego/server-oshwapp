6cc4b21fc399781b742105ac0c8d3a140d6e7891
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-07 17:43:54 +0200

    delete questionnaire along with user

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -2,15 +2,17 @@ const db = require('../db.js');
 
 module.exports = {
 
-    async getAllUsers(req, res){
+    async getAllUsers(req, res) {
         try {
             const users = await db.all('User')
             const json = await users.toJson()
-            const result = json.map( user => { return { 
-                username: user.username,
-                uuid: user.uuid,
-                // manages: user.manages
-            }})
+            const result = json.map(user => {
+                return {
+                    username: user.username,
+                    uuid: user.uuid,
+                    // manages: user.manages
+                }
+            })
             res.status(200).send(result)
             // console.log(json)
         } catch (error) {
@@ -21,15 +23,17 @@ module.exports = {
         }
     },
 
-    async getAllProjects(req, res){
+    async getAllProjects(req, res) {
         try {
             const projects = await db.all('Project')
             const json = await projects.toJson()
-            const results = json.map( project => { return { 
-                name: project.name,
-                uuid: project.uuid,
-                // manages: user.manages
-            }})
+            const results = json.map(project => {
+                return {
+                    name: project.name,
+                    uuid: project.uuid,
+                    // manages: user.manages
+                }
+            })
             res.status(200).send(results)
             // console.log(json)
         } catch (error) {
@@ -43,6 +47,15 @@ module.exports = {
     async deleteUser(req, res) {
         try {
             const user = await db.model('User').find(req.params.id)
+            const userJson = await user.toJson()
+
+            // delete questionnaire
+            // check first relationship "fills_in" is present otherwise gives errors and stops
+            if(userJson.fills_in){
+                const questionnaire = await db.model('Questionnaire').find(userJson.fills_in.node.uuid)
+                await questionnaire.delete()
+            }
+
             await user.delete()
             res.status(200).send({ msg: `user with uuid:${req.params.id} has been deleted` });
         } catch (error) {
