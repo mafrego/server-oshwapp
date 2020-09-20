d4e1790cd8cc960b5741c94ff7ee47d907dfd0be
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-20 17:33:10 +0200

    add imageUrl

--- src/controllers/AtomsController.js
+++ src/controllers/AtomsController.js
@@ -55,13 +55,11 @@ module.exports = {
 
     async addAtomToBom(req, res) {
         try {
-            // console.log(req.body)
-            let atom = req.body
-            // TODO add s3 image file path to atom
-            // TODO add itemNumber and quantity_to_assemble at client side
-            atom.quantity_to_assemble = atom.quantity    
-            // console.log(req.params.projectID)
+            console.log(req.params.projectID)
             const projectID = req.params.projectID
+            let atom = req.body
+            atom.imageUrl = "https://oshwapp.s3.eu-central-1.amazonaws.com/"+projectID+"/images/"+atom.name+".png"
+            console.log(atom)
             const response = await Promise.all([
                 db.find('Project', projectID),
                 db.create('Atom', atom)
