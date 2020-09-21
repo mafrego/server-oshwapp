6319712008895a486ab8e166c833b41fe1fa132b
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-21 13:53:05 +0200

    added isEmail()

--- src/service/regex.js
+++ src/service/regex.js
@@ -4,6 +4,11 @@ const iso31662 = require("iso-3166/2")
 
 module.exports = {
 
+    isEmail(str) {
+        const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return pattern.test(str)
+    },
+
     isAlphanumericString(str) {
         const pattern = /^[-0-9a-zA-Z_]+$/;     //plus hyphens and underscores
         return pattern.test(str)
