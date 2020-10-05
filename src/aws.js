371f4136e93fa075c63d18cc5613a0191d12e9ad
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-10-05 17:51:26 +0200

    deprecated aws properties removed

--- src/aws.js
+++ src/aws.js
@@ -5,10 +5,11 @@ const AWSAccessKeyId = process.env.AWSAccessKeyId
 const AWSSecretKey = process.env.AWSSecretKey
 
 if (process.env.NODE_ENV === 'production') {
-    aws.config = new aws.Config();
-    aws.config.accessKeyId = AWSAccessKeyId
-    aws.config.secretAccessKey = AWSSecretKey
-    aws.config.region = 'eu-central-1'
+    aws.config = new aws.Config({
+        accessKeyId: AWSAccessKeyId, 
+        secretAccessKey: AWSSecretKey, 
+        region: 'eu-central-1'
+    });
 } else {
     aws.config.update({
         accessKeyId: AWSAccessKeyId,
